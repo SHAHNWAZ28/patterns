67124c799f0d2c5ef96da3dc9b1e654116ec99e9
SHAHNWAZ HUSSAIN <153744620+SHAHNWAZ28@users.noreply.github.com>
2024-01-16 23:19:40 +0530

    Update javaBasics_pattern.java

--- javaBasics_pattern.java
+++ javaBasics_pattern.java
@@ -1,28 +1,5 @@
 import java.util.*;
 public class javaBasics{
-    public static void binToDec(int n){
-        int pow = 0;
-        int dec = 0;
-        while (n > 0){
-            int lastdigit = n%10;
-            dec = dec + (lastdigit* (int)Math.pow(2,pow));
-            pow++;
-            n = n/10;
-        }
-        System.out.print("decimal number is = "+ dec );
-    }
-    public static void decToBin(int n){
-        int pow = 0;
-        int binNum = 0;
-        while(n > 0){
-            int rem = n % 2;
-            binNum  = binNum +( rem*(int)Math.pow(10,pow));
-            pow++;
-            n = n/2;
-        }
-        System.out.println(" binary number is =" + binNum);
-
-    }
     public static void hollow_rectangle(int totRows, int totColm){
         // outer loop
         for(int i = 1; i<=totRows; i++){
