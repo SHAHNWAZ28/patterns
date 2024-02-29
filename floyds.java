c2c1879f51af2b921f728f07d209e36de13a30e4
SHAHNWAZ HUSSAIN <153744620+SHAHNWAZ28@users.noreply.github.com>
2024-02-29 19:21:44 +0530

    Add files via upload

---
+++ floyds.java
@@ -0,0 +1,19 @@
+public class floyds {
+    public static void floyds_triangle(int n){
+        int counter= 1;
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print(counter+" ");
+                counter++;
+            }
+            System.out.println();
+
+        }
+        
+    }
+    public static void main(String args[]){
+        floyds_triangle(10);
+
+    }
+    
+}
