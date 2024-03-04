bd0089437103cbe9feee6f89459683a8a20df300
SHAHNWAZ HUSSAIN <153744620+SHAHNWAZ28@users.noreply.github.com>
2024-03-05 00:14:22 +0530

    Array in java

---
+++ LinearSearch.java
@@ -0,0 +1,24 @@
+public class LinearSearch {
+    public static int Linear(int number[], int key){
+        for(int i = 0; i< number.length; i++){
+            if(number[i]== key){
+                return i;
+            }
+        }
+        return -1;
+
+    }
+    public static void main(String args[]){
+        int number[] = {2,4,6,8,10,12,14,16};
+        int key = 10;
+        int index = Linear(number,key);
+        if(index==-1){
+            System.out.println("not found");
+        }
+        else{
+            System.out.println("key index is found at :"+ index);
+        }
+
+    }
+    
+}
